bd7c3046ec1d30023b376ab12c1dee5cfa97caf1
26huitailang <26huitailang@gmail.com>
2016-09-05 18:01:08 +0800

    flask official tutorial

--- config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# configuration
-DATABASE = '/tmp/myblog.db'
-DEBUG = True
-SECRET_KEY = 'dont try to guess my secret key'
-USERNAME = 'admin'
-PASSWORD = 'default'
