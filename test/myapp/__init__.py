4fb5165b513ae4273034191fdf9fc2fc3919393a
26huitailang <26huitailang@gmail.com>
2017-03-15 13:57:05 +0800

    feat: 单应用变为蓝图，直接初始化app改为create_app()方法在run中调用
    1. 注意db等对象实例化时，先在create_app前实例化，然后在方法中绑定到app

--- test/myapp/__init__.py
+++ test/myapp/__init__.py
@@ -1,16 +1,28 @@
 # coding: utf-8
 import os
-from flask_sqlalchemy import SQLAlchemy
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .main import main
+from .ezbp import ezbp
 
-app = Flask(__name__)
-app.config.from_object('config')
+db = SQLAlchemy()
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-# 声明ORM底层所用数据库的访问URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
-# 当关闭数据库连接时是否自动提交事务
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-db = SQLAlchemy(app)
 
-from . import views, models
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    # 声明ORM底层所用数据库的访问URL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.db')
+    # 当关闭数据库连接时是否自动提交事务
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+    db.init_app(app)
+
+    app.register_blueprint(ezbp, url_prefix='/ezbp')
+    app.register_blueprint(main, url_prefix='/')
+    return app
+
+from . import models
