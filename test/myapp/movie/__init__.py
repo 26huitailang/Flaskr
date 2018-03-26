a606dd91ec13e1d69f576d6fc1241861bf159843
26huitailang <26huitailang@gmail.com>
2018-03-26 23:44:30 +0800

    实验前端select的动态管理

--- test/myapp/movie/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-movie = Blueprint('movie', __name__,
-                  template_folder='templates',
-                  static_folder='static')
-
-from . import views
--- test/run.py
+++
@@ -1,4 +0,0 @@
-from myapp import create_app
-
-app = create_app()
-app.run('0.0.0.0', port=8000, debug=True)
